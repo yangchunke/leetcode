7eb2af7743073c4d49fc6fce4bcf7db8cf1afa2a
chunkeya <chunkeya@YCKX1.redmond.corp.microsoft.com>
2014-09-01 16:42:27 -0700

    Gas Station O(n)

--- src/net/remyang/leetcode/GasStation.java
+++ src/net/remyang/leetcode/GasStation.java
@@ -2,12 +2,38 @@ package net.remyang.leetcode;
 
 public class GasStation {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 	public int canCompleteCircuit(int[] gas, int[] cost) {
+	    
+        int available = 0;
+        int all = 0;
+        int localMaxIndex = -1;
+        int globalMax = Integer.MIN_VALUE;
+        int globalMaxIndex = -1;
+        
+        for(int i=0;i<gas.length*2;i++){
+            int ii = i%gas.length;
+            all += gas[ii]-cost[ii];
+            available += gas[ii]-cost[ii];
+            if (available>=0){
+                if (localMaxIndex == -1){
+                    localMaxIndex = ii;
+                }
+                if (available>globalMax){
+                    globalMax = available;
+                    globalMaxIndex = localMaxIndex;
+                }
+            }
+            else
+            {
+                available = 0;
+                localMaxIndex = -1;
+            }
+        }
+        
+        return all>=0?globalMaxIndex:-1;
+    }
+	
+	public int canCompleteCircuitON2(int[] gas, int[] cost) {
         
         for(int i=0;i<gas.length;i++){
             if (canReach(gas, cost, i)){
